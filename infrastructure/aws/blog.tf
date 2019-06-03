d2cc6d16d39b369ca434f6d25a8e8a5c4ad439c1
Josh Barker <josh.barker.developer@gmail.com>
2019-06-03 21:53:34 +1000

    Upload blog after s3 bucket exists

--- infrastructure/aws/blog.tf
+++ infrastructure/aws/blog.tf
@@ -2,6 +2,6 @@ resource "null_resource" "upload_blog" {
   count = "${var.blog_source_path != "" ? 1 : 0 }"
 
   provisioner "local-exec" {
-    command = "aws s3 sync ${var.blog_source_path} s3://${local.bucket_name}"
+    command = "aws s3 sync ${var.blog_source_path} s3://${aws_s3_bucket.www.id}"
   }
 }
