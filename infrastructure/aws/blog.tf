9bd03ab18ba77eb4290eabe8b93c0427fdb99fc4
Josh Barker <josh.barker.developer@gmail.com>
2019-06-03 21:26:25 +1000

    Upload blog using aws cli

--- infrastructure/aws/blog.tf
+++ infrastructure/aws/blog.tf
@@ -1,10 +1,5 @@
-resource "aws_s3_bucket_object" "object" {
-  bucket = "${aws_s3_bucket.www.id}"
-
-  key    = "index.html"
-  source = "files/index.html"
-
-  content_type = "text/html"
-
-  etag = "${filemd5("files/index.html")}"
+resource "null_resource" "upload_blog" {
+  provisioner "local-exec" {
+    command = "aws s3 sync ${var.blog_source_path} s3://${local.bucket_name}"
+  }
 }
