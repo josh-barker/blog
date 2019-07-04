f069f54920da682096109e4cfd9d1ffa365d4b11
Josh Barker <josh.barker.developer@gmail.com>
2019-07-04 22:11:55 +1000

    Upload blog every deploy

--- infrastructure/aws/blog.tf
+++ infrastructure/aws/blog.tf
@@ -1,6 +1,10 @@
 resource "null_resource" "upload_blog" {
   count = "${var.blog_source_path != "" ? 1 : 0 }"
 
+  triggers {
+    build_number = "${timestamp()}"
+  }
+
   provisioner "local-exec" {
     command = "aws s3 sync ${var.blog_source_path} s3://${aws_s3_bucket.www.id}"
   }
