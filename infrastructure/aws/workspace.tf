6d6fe3062d04b543f75c926214cacf880e873f57
Josh Barker <josh.barker.developer@gmail.com>
2020-02-12 22:24:36 +1100

    Update workspace id

--- infrastructure/aws/workspace.tf
+++ infrastructure/aws/workspace.tf
@@ -2,5 +2,5 @@ resource "tfe_variable" "confirm_destroy" {
   key          = "CONFIRM_DESTROY"
   value        = "1"
   category     = "env"
-  workspace_id = "blog/${var.branch}"
+  workspace_id = "blog/${terraform.workspace}"
 }
