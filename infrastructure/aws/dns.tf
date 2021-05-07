e00941abce03e32bdaa56608ff5a1b831f5c45c3
Josh Barker <josh.barker.developer@gmail.com>
2021-05-07 19:28:58 +1000

    fix: fix root domain redirect

--- infrastructure/aws/dns.tf
+++ infrastructure/aws/dns.tf
@@ -22,3 +22,27 @@ resource "aws_route53_record" "www" {
   }
 }
 
+resource "aws_route53_record" "root_domain" {
+  zone_id = data.aws_route53_zone.external.zone_id
+  name    = var.root_domain
+  type    = "A"
+
+  alias {
+    name = local.enable_cdn == 1 ? element(
+      concat(
+        aws_cloudfront_distribution.www_distribution.*.domain_name,
+        [""],
+      ),
+      0,
+    ) : aws_s3_bucket.www.website_domain
+    zone_id = local.enable_cdn == 1 ? element(
+      concat(
+        aws_cloudfront_distribution.www_distribution.*.hosted_zone_id,
+        [""],
+      ),
+      0,
+    ) : aws_s3_bucket.www.hosted_zone_id
+
+    evaluate_target_health = false
+  }
+}
